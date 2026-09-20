6a39533835c4dff41dda80f0f1cb8863a667b4d0
Samyak <i.a.samyakr.sakhare05@gmail.com>
2026-09-20 11:45:40 +0530

    Added the button in js

--- script.jsx
+++ script.jsx
@@ -1,2 +1,10 @@
 // Javascript
 console.log("Javascript is successfully linked!");
+
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function(){
+    button.style.backgroundColor = 'purple';
+    button.textContent = 'Clicked!';
+    alert('You interacted with the page!');
+})
